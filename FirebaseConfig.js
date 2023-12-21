8ced6da871555975fd533a1580cb205e145aead7
Koray Can Yılmaz <krycnylmz@gmail.com>
2023-12-21 03:10:49 +0300

    readme file

---
+++ FirebaseConfig.js
@@ -0,0 +1,14 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import {getFirestore} from "firebase/firestore"
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {};
+
+// Initialize Firebase (We ensure that initialized before we use it)
+export const FIREBASE_APP = initializeApp(firebaseConfig);
+export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
+
+// export const FIREBASE_ANALYTICS = getAnalytics(FIREBASE_APP);
